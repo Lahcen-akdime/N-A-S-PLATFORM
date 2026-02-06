815f92bccb2e32fd8fb019c5449ef6acc432ed59
Lahcen <akdiml309@gmail.com>
2026-02-06 11:19:54 +0100

    add : Login Controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index(){
+
+    }
+    public function show(){
+        
+    }
+    public function Create(){
+        
+    }
+    public function Store(){
+        
+    }
+}
